daefdd3244719c0281e6c62e02422254393b7f61
Rafael <rafaelmassaki242@gmail.com>
2024-11-21 22:36:48 -0300

    commitzudo

--- novo_produto.php
+++ novo_produto.php
@@ -70,23 +70,37 @@
         $datavalidade = "";
     }
 ?>
+<br>
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">
+                Cadastrar novo produto
+            </div>
+            <div class="card-body">
+                <form action="novo_produto.php" method="post">
+                    <label>Nome</label>
+                    <input class="form-control" type="text" name="nome" value="<?php echo $nome; ?>" />
+                    <br>
+                    <label>Valor</label>
+                    <input class="form-control" type="number" name="valor" value="<?php echo $valor; ?>" />
+                    <br>
+                    <label>Código de barras</label>
+                    <input class="form-control" type="text" name="codigobarras" value="<?php echo $codigobarras; ?>" />
+                    <br>
+                    <label>Data de Validade</label>
+                    <input class="form-control" type="date" name="datavalidade" value="<?php echo $datavalidade; ?>" />
+                    <br>
+                    <button type='submit' class='btn btn-success'>
+                        Enviar os dados
+                    </button>
+                </form>
+            </div>
+        </div>    
 
-<form action="novo_produto.php" method="post">
-    <label>Nome</label>
-    <input type="text" name="nome" value="<?php echo $nome; ?>" />
-    <br>
-    <label>Valor</label>
-    <input type="number" name="valor" value="<?php echo $valor; ?>" />
-    <br>
-    <label>Código de barras</label>
-    <input type="text" name="codigobarras" value="<?php echo $codigobarras; ?>" />
-    <br>
-    <label>Data de Validade</label>
-    <input type="date" name="datavalidade" value="<?php echo $datavalidade; ?>" />
-    <br>
-    <button type='submit' class='btn btn-success'>
-        Enviar os dados
-    </button>
-</form>
 
+    </div>
+    <div class="col-4"></div>
+</div>
 <?php include "rodape.php"; ?>
--- produtos.php
+++ produtos.php
@@ -1,14 +1,14 @@
 <?php include "cabecalho.php"; ?>
- 
-<?php
+
+<?php 
 if( isset($_GET["pesquisa"]) )
 {
     $pesquisa = $_GET["pesquisa"];
     if( empty($pesquisa) )
     {
-       //Se a variavel estiver vazia executa aqui
+       //Se a variavel estiver vazia executa aqui 
        include "conexao.php";
-       $sql = "Select Id, Descricao, Valor, Codigo_barra, Imagem , categoria_Id from Produtos order by Id desc";
+       $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem, Categoria_Id from Produtos order by Id desc";
        $resultado = $conexao->query($sql);
        
        $conexao->close();
@@ -17,12 +17,12 @@ if( isset($_GET["pesquisa"]) )
     {
         //Aqui vai a lógica da pesquisa
         include "conexao.php";
-        $sql = "Select Id, Descricao, Valor, Codigo_barra, Imagem, cadegoria_Id
+        $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem, Categoria_Id 
                 from Produtos  
-                where Descricao like '%$pesquisa%' || Codigo_Barra = '$pesquisa'
+                where Descricao like '%$pesquisa%' || Codigo_Barras = '$pesquisa'
                 order by Id desc";
         $resultado = $conexao->query($sql);
-       
+        
         $conexao->close();
     }
 }
@@ -30,23 +30,32 @@ else
 {
     $pesquisa = "";
     include "conexao.php";
-    $sql = "Select P.Id, P.Descricao, P.Valor, P.Codigo_barra, P.Imagem , P.categoria_Id , C.Nome from Produtos P left join Categorias C ON (P.categoria_Id = C.Id) order by Id desc";
+    $sql = 
+    "Select P.Id, P.Descricao,
+             P.Valor, P.Codigo_barras, 
+             P.Imagem, P.Categoria_Id,
+             C.Nome
+            from Produtos P left join Categorias C
+            ON ( P.Categoria_Id = C.Id )
+            order by P.Id desc";
     $resultado = $conexao->query($sql);
    
     $conexao->close();
-   
+    
 }
- 
- 
+
+
 ?>
 <br>
 <?php
-if(isset ($_GET["erro"]) && !empty($_POST["erro"])){
-    echo "<div class= 'alert alert danger'>";
-    echo $_GET["erro"];
-    echo "</div> ";
-}
+    if(isset($_GET["erro"]) && !empty($_GET["erro"]) )
+    {
+        echo "<div class='alert alert-danger'>";
+        echo $_GET["erro"];
+        echo "</div>";
+    }
 ?>
+<br>
 <div class="row">
     <div class="col-12">
         <div class="card">
@@ -63,13 +72,13 @@ if(isset ($_GET["erro"]) && !empty($_POST["erro"])){
                     <div class="col-8">
                         <form action="produtos.php" method="get">
                             <div class="input-group mb-3">
-                                <input type="text"
-                                        name="pesquisa"
+                                <input type="text" 
+                                        name="pesquisa" 
                                         value="<?php echo $pesquisa; ?>"  
-                                        class="form-control"
+                                        class="form-control" 
                                         placeholder="Digite sua pesquisa aqui..." />
- 
- 
+
+
                                 <button class="btn btn-primary" type="submit">
                                     Pesquisar
                                 </button>
@@ -78,10 +87,10 @@ if(isset ($_GET["erro"]) && !empty($_POST["erro"])){
                     </div>
                     <div class="col-2"></div>
                 </div>
- 
+
                 <div class="row">
                     <div class="col-12">
-                       
+                        
                         <table class="table table-hover table-striped">
                         <thead>
                             <tr>
@@ -94,18 +103,18 @@ if(isset ($_GET["erro"]) && !empty($_POST["erro"])){
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
-                           
+                            <?php 
+                            
                             if ($resultado->num_rows > 0) {
                                 while($row = $resultado->fetch_assoc()) {
                                     echo "<tr>";
                                     echo "<td>" . $row["Id"] . "</td>";
                                     echo "<td>" . $row["Descricao"] . "</td>";
                                     echo "<td>" . $row["Valor"] . "</td>";
-                                    echo "<td>" . $row["Codigo_barra"] . "</td>";
+                                    echo "<td>" . $row["Codigo_barras"] . "</td>";
                                     echo "<td>" . $row["Imagem"] . "</td>";
                                     echo "<td>" . $row["Nome"] . "</td>";
-                                 
+                                    
                                     echo "<td><a href='editar_produto.php?Id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
                                     echo "<a href='excluir_produto.php?Id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
                                     echo "</tr>";
@@ -114,16 +123,15 @@ if(isset ($_GET["erro"]) && !empty($_POST["erro"])){
                                 echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
                             }
                             ?>
-                                                   
+                                                    
                         </tbody>
                         </table>
                     </div>
                 </div>
- 
+
             </div>
         </div>
     </div>
 </div>
- 
+
 <?php include "rodape.php"; ?>
- 
